3ee39caa703a47ea74efc9b710ac2acea3b10d21
Villimon <sidorov.oleg98@mail.ru>
2025-07-14 15:40:15 +0300

    Добавил информацию на главную страницу

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,3 +1,13 @@
 import { memo } from 'react'
+import { useTranslation } from 'react-i18next'
+import cls from './MainPage.module.scss'
 
-export const MainPage = memo(() => <main>MAIN PAGE</main>)
+export const MainPage = memo(() => {
+    const { t } = useTranslation('')
+
+    return (
+        <main className={cls.container}>
+            {t('Информация на главной странице')}
+        </main>
+    )
+})
